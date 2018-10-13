7504cc9d2122f156d1612819d008bea165c497ea
Shoraki, Mohsen <Mohsen.Shoraki1@T-Mobile.com>
2018-10-13 18:36:41 -0500

    initial upload the prokect in repo.

---
+++ springandthymleaf/src/main/java/com/amarillo/springandthymleaf/SpringAndThymleafApplication.java
@@ -0,0 +1,12 @@
+package com.amarillo.springandthymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndThymleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAndThymleafApplication.class, args);
+    }
+}
