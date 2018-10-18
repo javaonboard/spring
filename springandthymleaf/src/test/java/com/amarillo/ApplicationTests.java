58a917b482b2ce1e0242732c5accb845f70eab2f
bfly <billfly@programmer.net>
2018-10-18 15:11:12 -0500

    Added a test to log transaction records.

--- springandthymleaf/src/test/java/com/amarillo/ApplicationTests.java
+++ springandthymleaf/src/test/java/com/amarillo/ApplicationTests.java
@@ -1,18 +1,19 @@
 package com.amarillo;
 
-import com.amarillo.Services.RevenueService;
-import com.amarillo.Services.RevenueServiceImpl;
 import com.amarillo.entity.Transaction;
 import com.amarillo.repository.RevenueRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static org.junit.Assert.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Slf4j
 public class ApplicationTests {
     @Autowired
     RevenueRepository repository;
@@ -22,8 +23,14 @@ public class ApplicationTests {
     }
 
     @Test
-    public void testDB() {
+    public void testDBcount() {
         long revs = repository.count();
         assertEquals(4l, revs);
+    }
+
+    @Test
+    public void testDBall() {
+        Iterable<Transaction> it = repository.findAll();
+        it.forEach(t -> log.info("\n" + t.toString()));
     }
 }
